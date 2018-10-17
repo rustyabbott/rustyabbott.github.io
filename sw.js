1d433c64fd663e26d9bab5318efe267ed0f97022
Rusty Abbott <rusty@hutchinson-island.com>
2018-10-17 19:51:47 -0400

    Add service worker

---
+++ sw.js
@@ -0,0 +1,39 @@
+const cacheName = 'dev-portfolio';
+const cacheFiles = [
+  '/',
+  '/index.html',
+  '/css/styles.css',
+  '/js/main.js',
+  '/img/github-icon.svg',
+  '/img/logo.jpg',
+  '/img/twitter-icon.svg'
+];
+
+self.addEventListener('install', event => {
+  console.log('Service Worker installing');
+  event.waitUntil(caches.open(cacheName).then(cache => {
+      return cache
+      .addAll(cacheFiles)
+      .catch(error => {
+        console.log('Caches.open failed with error: ' + error);
+      })
+    })
+  )
+})
+
+self.addEventListener('activate', event => {
+  console.log('Service Worker activating');
+})
+
+self.addEventListener('fetch', event => {
+  if (event.request.method != 'GET') return;
+  event.respondWith(async function() {
+    const cache = await caches.open('retaurant-reviews-app');
+    const cachedResponse = await cache.match(event.request);
+    if (cachedResponse) {
+      event.waitUntil(cache.add(event.request));
+      return cachedResponse;
+    }
+    return fetch(event.request);
+  }());
+});
